29ad6b9a9517e958a12eb29376b1c996c9282b14
iatsuta <terraghost@mail.ru>
2026-05-27 12:49:54 +0200

    upd

--- src/Testing/Anch.Testing.Xunit/Engine/AnchTheoryTestMethod.cs
+++ src/Testing/Anch.Testing.Xunit/Engine/AnchTheoryTestMethod.cs
@@ -4,8 +4,6 @@ namespace Anch.Testing.Xunit.Engine;
 
 public class AnchTheoryTestMethod : XunitTestMethod, IXunitTestMethod
 {
-    private readonly IXunitTestMethod baseMethod;
-
     private readonly IServiceProviderPool? serviceProviderPool;
 
     public AnchTheoryTestMethod()
@@ -15,13 +13,12 @@ public class AnchTheoryTestMethod : XunitTestMethod, IXunitTestMethod
     public AnchTheoryTestMethod(IXunitTestMethod baseMethod, IServiceProviderPool? serviceProviderPool)
         : base(baseMethod.TestClass, baseMethod.Method, baseMethod.TestMethodArguments, baseMethod.UniqueID)
     {
-        this.baseMethod = baseMethod;
         this.serviceProviderPool = serviceProviderPool;
     }
 
     IReadOnlyCollection<IDataAttribute> IXunitTestMethod.DataAttributes => field ??=
     [
-        .. baseMethod.DataAttributes.Select(attr =>
+        .. base.DataAttributes.Select(attr =>
         {
             if (attr is IServiceProviderPoolAttribute serviceProviderPoolAttribute)
             {
@@ -38,11 +35,11 @@ public class AnchTheoryTestMethod : XunitTestMethod, IXunitTestMethod
         object?[]? testMethodArguments,
         Type[]? methodGenericTypes)
     {
-        var displayName = baseMethod.GetDisplayName(baseDisplayName, label, testMethodArguments, methodGenericTypes);
+        var displayName = base.GetDisplayName(baseDisplayName, label, testMethodArguments, methodGenericTypes);
 
-        if (testMethodArguments != null && baseMethod.Method.LastParameterIsCt() && baseMethod.Method.LastParameterIsCt() && displayName.EndsWith("???)"))
+        if (testMethodArguments != null && base.Method.LastParameterIsCt() && base.Method.LastParameterIsCt() && displayName.EndsWith("???)"))
         {
-            var skipPattern = $", {baseMethod.Method.GetParameters().Last().Name}: ???)";
+            var skipPattern = $", {base.Method.GetParameters().Last().Name}: ???)";
 
             if (displayName.EndsWith(skipPattern))
             {
